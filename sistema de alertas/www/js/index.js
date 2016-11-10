8eb1be8b7ca8c037049c938c4fffc4fd1b0f0915
Aeon1 <joel.r@live.com.mx>
2016-11-10 13:30:43 -0700

    prueba audio 3

--- sistema de alertas/www/js/index.js
+++ sistema de alertas/www/js/index.js
@@ -1452,10 +1452,25 @@ function onBackKeyDown(){
 }
 function cau(){
   window.plugins.audioRecorderAPI.record(function(msg) {
-    // complete 
-    alert('ok: ' + msg);
-  }, function(msg) {
-    // failed 
-    alert('ko: ' + msg);
-  }, 30); // record 30 seconds 
+    window.plugins.audioRecorderAPI.record(function(savedFilePath) {
+  var fileName = savedFilePath.split('/')[savedFilePath.split('/').length - 1];
+  var directory;
+  if (cordova.file.documentsDirectory) {
+    directory = cordova.file.documentsDirectory; // for iOS 
+  } else {
+    directory = cordova.file.externalRootDirectory; // for Android 
+  }
+  $cordovaFile.copyFile(
+    cordova.file.dataDirectory, fileName,
+    directory, "new_file.m4a"
+  )
+    .then(function (success) {
+      alert(JSON.stringify(success));
+    }, function (error) {
+      alert(JSON.stringify(error));
+    });
+}, function(msg) {
+  alert('ko: ' + msg);
+}, 3);
+}
 }
